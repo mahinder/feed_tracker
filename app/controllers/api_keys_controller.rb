68ed9dd88d8fe5f2338e141206d772561c7bd50c
mahi <mahi@mahi-System-Product-Name.(none)>
2013-04-14 19:27:50 +0530

    update

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,7 +1,6 @@
 class ApiKeysController < ApplicationController
 	before_filter :authenticate_user!  
-	load_and_authorize_resource
-  
+ 
   def index
     
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
     rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, :alert => exception.message
+      redirect_to main_app.root_path, :flash => { :error => exception.message } 
     end
    def protect_admin_access
     if user_signed_in?
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
         redirect_to index_path
       end  
     else
-      redirect_to :controller => '/api_keys', :action => 'index'
+      redirect_to root_url
     end
   end   
 end
