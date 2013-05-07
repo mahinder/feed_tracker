89b1f99fb611af050e0d426ede9e25ac91a7f200
mahinder <mahinder.kumar@trantorinc.com>
2013-05-07 15:05:47 +0530

    update

--- db/migrate/20111207101142_create_news_types.rb
+++ db/migrate/20111207101142_create_news_types.rb
@@ -3,45 +3,6 @@ class CreateNewsTypes < ActiveRecord::Migration
     create_table :news_types do |t|
       t.string :name
     end
-    
-     new_news_types = []
-    new_news_types << (NewsType.find_or_create_by_name :name => "Awards")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Bankruptcy")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Earnings Release")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Executive Activity")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Funding")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Financial")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Acquisition")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Product")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Joined Board")
-    new_news_types << (NewsType.find_or_create_by_name :name => "New Company")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Re-Organization")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Layoffs")
-
-    news_type_map = {
-        'Acquisition' => 'Acquisition',
-        'Job change' => 'Executive Activity',
-        'Joined management' => 'Executive Activity',
-        'Joined management as VP Marketing' => 'Executive Activity',
-        'Recently funded' => 'Funding'
-    }
-
-    news_type_map.each_pair do |key, value|
-      old_news_type = NewsType.find_by_name(key)
-      news_type = NewsType.find_by_name value
-
-      if old_news_type
-        News.update_all("news_type_id = #{news_type.id}", "news_type_id = #{old_news_type.id}")
-        InterestingNewsType.update_all("news_type_id = #{news_type.id}", "news_type_id = #{old_news_type.id}")
-        NewsFeed.update_all("news_type_id = #{news_type.id}", "news_type_id = #{old_news_type.id}")
-      end
-    end
-
-    new_news_type_ids = new_news_types.collect(&:id)
-    NewsType.delete_all(['id NOT IN (?)', new_news_type_ids])
-    
-    
-    
   end
 
   def self.down
--- db/migrate/20131031062317_update_news_types_for_news.rb
+++ db/migrate/20131031062317_update_news_types_for_news.rb
@@ -1,18 +1,12 @@
 class UpdateNewsTypesForNews < ActiveRecord::Migration
   def self.up
-    new_news_types = []
-    new_news_types << (NewsType.find_or_create_by_name :name => "Awards")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Bankruptcy")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Earnings Release")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Executive Activity")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Funding")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Financial")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Acquisition")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Product")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Joined Board")
-    new_news_types << (NewsType.find_or_create_by_name :name => "New Company")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Re-Organization")
-    new_news_types << (NewsType.find_or_create_by_name :name => "Layoffs")
+    new_news_type_ids = []
+    
+    news_types = ["Awards", "Bankruptcy", "Earnings Release", "Funding", "Financial", "Acquisition", "Product", "Joined Board", "New Company", "Re-Organization", "Layoffs"]
+    
+    news_types.each do |news_type|
+      new_news_type_ids << NewsType.where(:name => news_type).create(:name => news_type).id
+    end
 
     news_type_map = {
         'Acquisition' => 'Acquisition',
@@ -24,7 +18,7 @@ class UpdateNewsTypesForNews < ActiveRecord::Migration
 
     news_type_map.each_pair do |key, value|
       old_news_type = NewsType.find_by_name(key)
-      news_type = NewsType.find_by_name value
+      news_type = NewsType.find_by_name(value)
 
       if old_news_type
         News.update_all("news_type_id = #{news_type.id}", "news_type_id = #{old_news_type.id}")
@@ -33,7 +27,6 @@ class UpdateNewsTypesForNews < ActiveRecord::Migration
       end
     end
 
-    new_news_type_ids = new_news_types.collect(&:id)
     NewsType.delete_all(['id NOT IN (?)', new_news_type_ids])
   end
 
